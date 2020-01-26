53ca7b7b5280c4928ea096abe97fe2d9f2522254
Lauren Yu <laurenkyu@gmail.com>
2020-01-26 01:24:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,12 @@
 def run_guessing_game
-  random number = rand(20) + 1 
+  random_number = rand(20) + 1 
+  puts "Hello there!  Make a guess!"
+  input = input.chomp 
+  if input == random_number 
+      puts "You guessed the correct number!"
+    elsif input = "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry!  The number guess #{random_number}."
+    end
 end 
