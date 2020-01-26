8ddfbd1ac7fede2cfb926fa64b7d7d4e420ecaca
Lauren Yu <laurenkyu@gmail.com>
2020-01-26 01:37:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,12 @@
 def run_guessing_game
   random_number = (rand(6) + 1).to_s
-  puts "Hello there!  Make a guess!"
+  p "Hello there!  Make a guess!"
   input = gets.chomp.to_s
   if input == random_number 
-      puts "You guessed the correct number!"
+      p "You guessed the correct number!"
     elsif input == "exit"
-      puts "Goodbye!"
+      p "Goodbye!"
     else
-      puts "Sorry!  The computer guessed #{random_number}.\n"
+      p "Sorry!  The computer guessed #{random_number}."
     end
 end 
